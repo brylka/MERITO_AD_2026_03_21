d6cb68a95bd6615dbb6c795a45377e424ac4de21
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-21 12:30:40 +0100

    Sobota

--- 5.py
+++ 5.py
@@ -11,22 +11,22 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
 
-model = xdb.XGBClassifier(
-    n_estimators=150,
-    max_depth=None,
-    learning_rate=0.01,
-    random_state=42)
+model = lgb.LGBMClassifier(
+    n_estimators=200,
+    learning_rate=0.1,
+    random_state=42, verbose=-1)
 model.fit(X_train, y_train)
 
 model_train_acc = model.score(X_train, y_train)
 model_test_acc = model.score(X_test, y_test)
+model_cv_scores = cross_val_score(model, X, y, cv=10)
 
-print("XGBOOST:")
+print("LightGBM:")
 print("-" * 40)
 print(f"Dokładność na zbiorze treningowym: {model_train_acc:.4f}")
 print(f"Dokładność na zbiorze testowym:    {model_test_acc:.4f}")
 
-model_cv_scores = cross_val_score(model, X, y, cv=10)
+
 print(f"Walidacja cv:")
 print(f"Średnia dokładność: {model_cv_scores.mean():.4f}")
 print(f"Odchylenie std:     {model_cv_scores.std():.4f}")
