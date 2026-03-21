aa6134902d8b970a715a2c8a012632f468a22d4c
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-21 09:06:42 +0100

    Sobota

--- 1.py
+++ 1.py
@@ -13,7 +13,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 single_tree = DecisionTreeClassifier(random_state=42)
 single_tree.fit(X_train, y_train)
 
-random_forest = RandomForestClassifier(random_state=42)
+random_forest = RandomForestClassifier(
+    n_estimators=250,
+    max_depth=10,
+    min_samples_split=2,
+    max_features='sqrt',
+    bootstrap=True,
+    random_state=42)
 random_forest.fit(X_train, y_train)
 
 train_acc = single_tree.score(X_train, y_train)
